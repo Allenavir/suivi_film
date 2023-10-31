76851d75000e3a0a415a2389c33ca60c1e89ffa1
Allenavir <hunter59282@live.fr>
2023-10-31 13:25:44 +0100

    add the social logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,27 @@
 import '../styles/Footer.css';
-function Footer() {    
+import insta from '../assets/instagram-logo.png';
+import codepen from '../assets/codepen-logo.png';
+import gitHub from '../assets/github-logo.png';
+
+function Footer() {
     return (
         <footer>
             <p id="rights">©2023 Nicolas Flinois. Tous droits réservés.</p>
             <div id="social">
                 <p>Suivre sur les réseaux</p>
                 <div>
-                    <a className="socialLogo" href="https://www.instagram.com/allenavir/">Instagram</a>
-                    <a className="socialLogo" href="https://codepen.io/Allenavir">CodePen</a>
-                    <a className="socialLogo" href="https://github.com/Allenavir">GitHub</a>
+                    <a className="socialLogo" href="https://www.instagram.com/allenavir/">
+                        <img src={insta} alt="Instagram"></img>
+                    </a>
+                    <a className="socialLogo" href="https://codepen.io/Allenavir">
+                        <img src={codepen} alt="CodePen"></img>
+                    </a>
+                    <a className="socialLogo" href="https://github.com/Allenavir">
+                        <img src={gitHub} alt="GitHub"></img>
+                    </a>
                 </div>
-
             </div>
-        </footer>              
+        </footer>
     )
 }
 
